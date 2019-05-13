067d02d18e0ef18e61d7c24683c466d43ab10db4
Valentin Boettcher <hiro@protagon.space>
2019-05-13 09:25:10 +0200

    fix readme -> markdown

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup
 
+
+def readme():
+    with open('Readme.md') as f:
+        return f.read()
+
 setup(name='SecondaryValue',
-      version='0.0.3',
+      version='0.0.4',
       description='A helper to calculate the gaussian error propagation.',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
       url='https://github.com/vale981/SecondaryValue',
       author='Valentin Boettcher',
       author_email='hiro@protagon.space',
       license='GPLv3.0',
       packages=['SecondaryValue'],
+      keywords='gaussian error propagation',
       install_requires=[
           'numpy',
           'sympy',
