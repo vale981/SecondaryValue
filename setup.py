f89a9a0fac02eb32571f0a3186387f72a853fb93
Valentin Boettcher <valentin@boettcher.cf>
2019-05-20 16:50:42 +0200

    Version Bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
         return f.read()
 
 setup(name='SecondaryValue',
-      version='0.1.7',
+      version='0.1.9',
       description='A helper to calculate the gaussian error propagation.',
       long_description=readme(),
       long_description_content_type='text/markdown',
